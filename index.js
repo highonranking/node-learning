f676093aed54a6f61d6fe38d859c7981e4760c51
abhinav dixit <abhinavdixit2306@gmail.com>
2020-05-21 10:11:14 +0530

    simple node module

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 //jshint esversion: 6
-var rect = {
-  perimeter: (x,y) => (2*(x+y)),
-  area: (x,y) => (x*y)
-};
+var rect = require('./rectangle');
 
 function solveRect(l,b) {
   console.log("Solving for rectangle with l = " + l + " and b = " + b);
---
+++ rectangle.js
@@ -0,0 +1,4 @@
+//jshint esversion:6
+
+exports.perimeter =  (x,y) => (2*(x+y));
+exports.area =  (x,y) => (x*y);
